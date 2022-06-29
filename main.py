055f13a41d9fb957210464c77ac7c66b99ea7b10
zhouhuajian <zhouhuajian@email.com>
2022-06-30 01:05:09 +0800

    数字挑战计算器 v1.0
    1. 完成1.0版的数字挑战计算器；
    2. 排列组合算法可以优化。

---
+++ main.py
@@ -0,0 +1,129 @@
+"""
+数字挑战计算器
+
+@author: zhouhuajian
+@version: v1.0
+"""
+
+
+class DigitChallengeCalculator:
+    """数字挑战计算器"""
+
+    def __init__(self):
+        """初始化"""
+        self.digits = list(range(1, 10))  # 1 2 3 4 5 6 7 8 9
+
+    def start_calculate(self, exp: str):
+        """开始计算"""
+        # TODO: 目前暂且只支持2-5个问号，排列组合算法可能可以优化
+        qmark_num = exp.count('?')
+        # 让表达式变成结果为布尔值的表达式
+        # 另外把?变成{}，这样可以使用字符串的format方法
+        try:
+            equal_sign_index = exp.index("=")
+        except ValueError:
+            raise ValueError("必须要有=号")
+        exp = exp[:equal_sign_index] + '=' + exp[equal_sign_index:]
+        exp = exp.replace("?", "{}")
+        result = ''
+        if qmark_num < 2:
+            raise ValueError("?必须2个以上")
+        elif qmark_num == 2:
+            result = self.calculate_qmark_2(exp)
+        elif qmark_num == 3:
+            result = self.calculate_qmark_3(exp)
+        elif qmark_num == 4:
+            result = self.calculate_qmark_4(exp)
+        elif qmark_num == 5:
+            result = self.calculate_qmark_5(exp)
+        # 两个等号变成一个
+        return result.replace("==", '=')
+
+    def calculate_qmark_2(self, exp: str):
+        """2个问号"""
+        for num_1 in self.digits:
+            digits_copy_for_num_2 = self.digits.copy()
+            digits_copy_for_num_2.remove(num_1)
+            for num_2 in digits_copy_for_num_2:
+                new_exp = exp.format(num_1, num_2)
+                # 如果执行后为True，则得到答案
+                if eval(new_exp):
+                    return new_exp
+
+    def calculate_qmark_3(self, exp):
+        """3个问号"""
+        for num_1 in self.digits:
+            digits_copy_for_num_2 = self.digits.copy()
+            digits_copy_for_num_2.remove(num_1)
+            for num_2 in digits_copy_for_num_2:
+                digits_copy_for_num_3 = self.digits.copy()
+                digits_copy_for_num_3.remove(num_1)
+                digits_copy_for_num_3.remove(num_2)
+                for num_3 in digits_copy_for_num_3:
+                    new_exp = exp.format(num_1, num_2, num_3)
+                    # 如果执行后为True，则得到答案
+                    if eval(new_exp):
+                        return new_exp
+
+    def calculate_qmark_4(self, exp):
+        """4个问号"""
+        for num_1 in self.digits:
+            digits_copy_for_num_2 = self.digits.copy()
+            digits_copy_for_num_2.remove(num_1)
+            for num_2 in digits_copy_for_num_2:
+                digits_copy_for_num_3 = self.digits.copy()
+                digits_copy_for_num_3.remove(num_1)
+                digits_copy_for_num_3.remove(num_2)
+                for num_3 in digits_copy_for_num_3:
+                    digits_copy_for_num_4 = self.digits.copy()
+                    digits_copy_for_num_4.remove(num_1)
+                    digits_copy_for_num_4.remove(num_2)
+                    digits_copy_for_num_4.remove(num_3)
+                    for num_4 in digits_copy_for_num_4:
+                        new_exp = exp.format(num_1, num_2, num_3, num_4)
+                        # 如果执行后为True，则得到答案
+                        if eval(new_exp):
+                            return new_exp
+
+    def calculate_qmark_5(self, exp):
+        """5个问号"""
+        for num_1 in self.digits:
+            digits_copy_for_num_2 = self.digits.copy()
+            digits_copy_for_num_2.remove(num_1)
+            for num_2 in digits_copy_for_num_2:
+                digits_copy_for_num_3 = self.digits.copy()
+                digits_copy_for_num_3.remove(num_1)
+                digits_copy_for_num_3.remove(num_2)
+                for num_3 in digits_copy_for_num_3:
+                    digits_copy_for_num_4 = self.digits.copy()
+                    digits_copy_for_num_4.remove(num_1)
+                    digits_copy_for_num_4.remove(num_2)
+                    digits_copy_for_num_4.remove(num_3)
+                    for num_4 in digits_copy_for_num_4:
+                        digits_copy_for_num_5 = self.digits.copy()
+                        digits_copy_for_num_5.remove(num_1)
+                        digits_copy_for_num_5.remove(num_2)
+                        digits_copy_for_num_5.remove(num_3)
+                        digits_copy_for_num_5.remove(num_4)
+                        for num_5 in digits_copy_for_num_5:
+                            new_exp = exp.format(num_1, num_2, num_3, num_4, num_5)
+                            # 如果执行后为True，则得到答案
+                            if eval(new_exp):
+                                return new_exp
+
+
+if __name__ == '__main__':
+    print("共9个数字 1 2 3 4 5 6 7 8 9")
+    print("不能重复使用")
+    print("请输入表达式，例如(?+?)*?=21")
+    calculator = DigitChallengeCalculator()
+    while True:
+        exp = input("Input: ")
+        try:
+            result = calculator.start_calculate(exp)
+            if not result:
+                print("没有正确答案，请检查输入是否正确")
+                continue
+        except Exception as e:
+            result = repr(e)
+        print("Output: " + result)
